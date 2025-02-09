c816818087357172297a964a027a0f43f5e9f058
Glas42 <145870870+OleFranz@users.noreply.github.com>
2025-02-09 14:06:56 +0100

    Author Name Change

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="ImageUI",
-    version="0.9",
+    version="0.10",
     description="A package for easily creating UIs in Python, mainly using OpenCV's drawing functions.",
     long_description=open("README.md").read(),
-    author="Glas42",
+    author="OleFranz",
     license="GPL-3.0",
     packages=["ImageUI"],
     python_requires=">=3.9",
